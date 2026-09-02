2ee28c513270a321424b48863f70a4e5b1d5863c
AymanSha3ban <aymankhattap483@gmail.com>
2026-09-02 11:53:01 +0300

    patch

--- src/apis/Auth/Users.api.ts
+++ src/apis/Auth/Users.api.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+
 import {API} from '../BaseAPI.ts';
 
 export const getUser = async (id: number) => {
@@ -6,12 +6,22 @@ export const getUser = async (id: number) => {
   return data;
 };
 export const getUsers = async () => {
-  const { data } = await axios.get("http://localhost:3000/users")
+  const { data } = await API.get("/users");
   return data;
 };
 
 export const getLoginedUser = async () => {
   const userId = localStorage.getItem('userId');
   const { data } = await API.get(`/users/${userId}`); 
+  return data;
+};
+
+export const patchUser = async (id: string, userData: Partial<import('../../interfaces/interfaces.ts').UserType>) => {
+  const { data } = await API.patch(`/users/${id}`, userData);
+  return data;
+};
+
+export const searchUsers = async (query: string) => {
+  const { data } = await API.get(`/users?q=${query}`);
   return data;
 };
